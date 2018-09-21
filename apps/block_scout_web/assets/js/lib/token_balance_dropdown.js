dca0f79723b1b445ed72240cbac6ba16e6b64788
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2018-09-21 17:49:10 -0300

    write javascript that controls the filter

--- apps/block_scout_web/assets/js/lib/token_balance_dropdown.js
+++ apps/block_scout_web/assets/js/lib/token_balance_dropdown.js
@@ -20,3 +20,38 @@ export function loadTokenBalanceDropdown () {
   $('[data-token-balance-dropdown]').each((_index, element) => tokenBalanceDropdown(element))
 }
 loadTokenBalanceDropdown()
+
+$(document).on('input', '[data-filter-dropdown-tokens]', event => {
+  const query = event.target.value.toLowerCase()
+  const $tokens = $('[data-dropdown-token-name]')
+  const $tokensCount = $('[data-tokens-count]')
+
+  $tokens.each((_index, token) => {
+    const $token = $(token)
+    const tokenName = $token.data('dropdown-token-name')
+    if (tokenName.toLowerCase().search(query) === -1) {
+      $token.addClass('d-none')
+    } else {
+      $token.removeClass('d-none')
+    }
+  })
+
+  $('[data-dropdown-token-type]').each((_index, container) => {
+    const $container = $(container)
+    const type = $container.data('dropdown-token-type')
+    const visibleChildrenCount = $container.children('[data-dropdown-token-name]:not(.d-none)').length
+
+    if (visibleChildrenCount === 0) {
+      $container.addClass('d-none')
+    } else {
+      $(`[data-dropdown-count-token-of-type='${type}']`).empty().append(visibleChildrenCount)
+      $container.removeClass('d-none')
+    }
+  })
+
+  $tokensCount.html($tokensCount.html().replace(/\d+/g, $tokens.not('.d-none').length))
+})
+
+$(document).on('hidden.bs.dropdown', '[data-token-balance-dropdown]', _event => {
+  $('[data-filter-dropdown-tokens]').val('').trigger('input')
+})
